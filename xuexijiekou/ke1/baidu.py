04734c235594762541a2bd7bc7b4d7e5c12e7382
yang-art <3256814316@qq.com>
2019-11-19 20:56:11 +0800

    百度URL

--- xuexijiekou/ke1/baidu.py
+++ xuexijiekou/ke1/baidu.py
@@ -1,8 +1,9 @@
 #coding:utf-8
 #__author__='易木'
 import requests
+#百度URL
 url = "https://www.baidu.com"
-r = requests.get(url,verify = False)
+r = requests.get(url,verify = False) #发送请求
 
 print(r.text) #出现乱码时候  è§åé¦</a>&nbsp;äº¬ICP
 print(r.content.decode('utf-8'))  #解码   content.decode(utf-8)
