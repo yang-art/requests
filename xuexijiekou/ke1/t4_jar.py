90c897f8a47331f1667ed544f6d803cc0addf14b
yang-art <3256814316@qq.com>
2019-11-19 23:25:21 +0800

    '获取cookies关联'

---
+++ xuexijiekou/ke1/t4_jar.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+#__author__='易木'
+
+import requests
+
+url = "https://www.chsi.com.cn"
+
+h = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+    'Upgrade-Insecure-Requests':'1'
+
+}
+
+r = requests.get(url,headers=h,verify = False)
+
+#RequestsCookieJar 格式的cookies
+coks = r.cookies
+print(coks)
+
+#jar的格式
+r2 =requests.get(url,headers=h,cookies=coks,verify=False)
+print(r2)
